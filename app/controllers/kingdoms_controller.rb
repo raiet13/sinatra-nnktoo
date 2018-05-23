2d4151e1dd569fe59e043bec989af8ce7371d859
Raiet Intraub <raietsaeed@gmail.com>
2018-05-23 20:10:08 +0000

    added kingdom new/show routes/action

--- app/controllers/kingdoms_controller.rb
+++ app/controllers/kingdoms_controller.rb
@@ -13,14 +13,33 @@ class KingdomsController < ApplicationController
   # Kingdom New Route #
   get '/kingdoms/new' do
     puts "New Kingdom Route"
+    @message = session[:error_message]
     redirect_if_not_logged_in
     erb :'/kingdoms/create_kingdom'
   end
 
   # Kingdom New Action #
-
+  post '/kingdoms' do
+    puts "New Kingdom Params = #{params}"
+    kingdom = Kingdom.new(name: params[:name], user_id: current_user.id)
+    if kingdom.save
+      puts "Save New Kingdom"
+      session[:error_message] = ""
+      redirect to "/kingdoms/#{kingdom.slug}"
+    else
+      puts "FAILURE TO SAVE KINGDOM"
+      session[:error_message] = "Something went wrong during kingdom creation please try again."
+      redirect to "/kingdoms/new"
+    end
+  end
 
   # Kingdom Show Route #
+  get '/kingdoms/:slug' do
+    puts "Kingdom Show Route"
+    redirect_if_not_logged_in
+    @kingdom = Kingdom.find_by_slug(params[:slug])
+    erb :'/kingdoms/show_kingdom'
+  end
 
 
   # Kingdom Edit Route #
