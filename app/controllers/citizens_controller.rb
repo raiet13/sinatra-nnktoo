a4941111d6448eb0233320dcd63fd722c453aa8b
Raiet Intraub <raietsaeed@gmail.com>
2018-05-23 22:37:48 +0000

    added citizen new route/action and view

--- app/controllers/citizens_controller.rb
+++ app/controllers/citizens_controller.rb
@@ -9,12 +9,27 @@ class CitizensController < ApplicationController
     erb :'/citizens/citizen_index'
   end
 
-
   # Citizen New Route #
-
+  get '/citizens/new' do
+    puts "New Citizen Route"
+    @message = session[:error_message]
+    redirect_if_not_logged_in
+    erb :'/citizens/create_citizen'
+  end
 
   # Citizen New Action #
-
+  post '/citizens' do
+    puts "New Citizen Params = #{params}"
+    citizen = Citizen.new(name: params[:name], role: params[:role])
+    if citizen.save
+      puts "Save New Citizen"
+      session[:error_message] = ""
+      # redirect to "/citizens/#{citizen.slug}"
+    else
+      puts "FAILURE TO SAVE CITIZEN"
+      session[:error_message] = "Something went wrong during citizen creation please try again."
+      redirect to "/citizens/new"
+    end
 
   # Citizen Show Route #
 
