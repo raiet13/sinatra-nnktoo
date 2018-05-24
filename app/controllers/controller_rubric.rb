e30ccb67b1d0c3e65f157aac83c219598d4e7e6f
Raiet Intraub <raietsaeed@gmail.com>
2018-05-24 01:06:37 +0000

    completed notes to self controller rubric

--- app/controllers/controller_rubric.rb
+++ app/controllers/controller_rubric.rb
@@ -4,13 +4,13 @@ class PLURALnameController < ApplicationController
 
   # Index Route #
   get '/name' do
-    # Check logged in or redirect to log in page
+    # Check logged in or redirect to log in view
     # Show index view for controller object
   end
 
   # New Route #
   get '/name/new' do
-    # Check logged in or redirect to log in page
+    # Check logged in or redirect to log in view
     # Show error message if necessary
     # Show creation view for controller object
   end
@@ -25,24 +25,34 @@ class PLURALnameController < ApplicationController
 
   # Show Route #
   get '/name/:param' do
+    # Check logged in or redirect to log in view
+    # Find object based on param (find using id or other param as defined as "find_by_X" in model)
     # Show error message if necessary
-
+    # Show show view for controller object
   end
 
   # Edit Route #
   get '/name/:param/edit' do
+    # Check logged in or redirect to log in view
     # Show error message if necessary
-
+    # Assemble any needed other object data as needed (via @variables for use in erb view)
+    # Show edit view for controller object
   end
 
   # Edit Action #
   post '/name/:param' do
-
+    # Find object based on param
+    # Update object using params hash from form post
+    # Save object
+    # Redirect to object's show view
   end
 
   # Delete Action #
   delete '/outfits/:slug/delete' do
-
+    # Find object
+    # Check if logged in (and that the correct user is trying to delete the object if/as neccessary)
+    # Delete (only if the above is true)
+    # Redirect to "main page" or whatever
   end
 
 
