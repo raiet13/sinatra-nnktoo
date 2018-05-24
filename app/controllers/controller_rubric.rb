f52904de48a6877224a8799d37bbeb163168a9d1
Raiet Intraub <raietsaeed@gmail.com>
2018-05-24 00:39:02 +0000

    added outfit new route/action/view

---
+++ app/controllers/controller_rubric.rb
@@ -0,0 +1,26 @@
+class PLURALnameController < ApplicationController
+
+  # Index Route #
+  get '/name' do
+
+  end
+
+  # New Route #
+  get '/name/new' do
+
+  end
+
+  # New Action #
+
+  # Show Route #
+
+  # Edit Route #
+
+  # Edit Action #
+
+  # Delete Action #
+
+
+end
+
+# rake db:migrate SINATRA_ENV=test
--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -9,8 +9,28 @@ class OutfitsController < ApplicationController
   end
 
   # Outfit New Route #
+  get '/outfits/new' do
+    # puts "New Outfit Route"
+    @message = session[:error_message]
+    redirect_if_not_logged_in
+    erb :'/outfits/create_outfit'
+  end
 
   # Outfit New Action #
+  post '/outfits' do
+    # puts "New Outfit Params = #{params}"
+    outfit = Outfit.new(name: params[:name], outfit_type: params[:outfit_type], user_id: current_user.id)
+
+    if outfit.save
+      # puts "Save New Outfit"
+      session[:error_message] = ""
+      redirect to "/outfits/#{outfit.slug}"
+    else
+      # puts "FAILURE TO SAVE Outfit"
+      session[:error_message] = "Something went wrong during outfit creation please try again."
+      redirect to "/outfits/new"
+    end
+  end
 
   # Outfit Show Route #
 
